121a0ec2152f00744cf4ccced9c97ae79ad9a677
robjar <robjar@wp.eu>
2017-07-23 18:10:51 +0200

    add /db route

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
-var app = express();
+var pg = require('pg');
 var cool = require('cool-ascii-faces');
+
 var TIMES = process.env.TIMES || 5;
+var DATABASE_URL = process.env.DATABSE_URL;
+var app = express();
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,18 +15,18 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.render('pages/index');
 });
 
-app.get('/cool', function(request, response) {
+app.get('/cool', function (request, response) {
   response.send(cool());
 });
 
-app.get('/times', function(request, response) {
+app.get('/times', function (request, response) {
   var result = '';
   var i;
-  
+
   for (i = 0; i < TIMES; i++) {
     result += i + ' ';
   }
@@ -30,8 +34,22 @@ app.get('/times', function(request, response) {
   response.send(result);
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/db', function (request, response) {
+  pg.connect(DATABASE_URL, function (err, client, done) {
+    client.query('SELECT * FROM test_table', function (err, result) {
+      done();
+      if (err) {
+        console.error(err);
+        response.send("Error " + err);
+      } else {
+        response.render('pages/db', {
+          results: result.rows
+        });
+      }
+    });
+  });
 });
 
-
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
